783ac7b0e362ba9315af055bfc4d35c7066e954a
Samuel1w <111092737+Samuel1w@users.noreply.github.com>
2025-11-17 05:38:02 +0100

    Update db.js

--- db.js
+++ db.js
@@ -4,14 +4,9 @@ dotenv.config(); // load .env first
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.PGUSER || 'postgres',
-  host: process.env.PGHOST || 'localhost',
-  database: process.env.PGDATABASE || 'Luxuria-db',
-  password: process.env.PGPASSWORD || 'dalina05',
-  port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432, // convert to number
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-// Test connection
-pool.connect()
-  .then(() => console.log('✅ Connected to PostgreSQL'))
-  .catch(err => console.error('❌ DB connection error:', err));
